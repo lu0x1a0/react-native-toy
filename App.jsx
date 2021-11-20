9981088dabeb7f6de45b528afa070b7644ec29c6
Sean Luo's Yoga <41530060+lu0x1a0@users.noreply.github.com>
2021-11-20 13:30:07 +1100

    before background

--- App.jsx
+++ App.jsx
@@ -1,27 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground} from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Home from './screens/Home.jsx'
+
+const image = { uri: "https://reactjs.org/logo-og.png" };
+
 export default function App() {
   return (
     <SafeAreaProvider>
-      <View style={[styles.container, {
-          // Try setting `flexDirection` to `"row"`.
-          flexDirection: "row"
-        }]}>
-        <View style={{ flex: 2, height:"100%", backgroundColor: "red" }}>
-          <Text>Red</Text>
+      <ImageBackground source={require('./assets/icon.png')} resizeMode="cover" style={styles.image}>
+        <View style={[styles.container, {
+            // Try setting `flexDirection` to `"row"`.
+            flexDirection: "row"
+          }]}>
+          <View style={{ flex: 2, height:"100%" }}>
+            <Text>Red</Text>
+          </View>
+          <View style={{ flex: 6, height:"100%" }}>
+            <Home time="2"/>
+          </View>
+          <View style={{ flex: 2, height:"100%" }}>
+            <Text>Orange</Text>
+          </View>
         </View>
-        <View style={{ flex: 6, height:"100%", backgroundColor: "green" }}>
-          <Home time="2"/>
-        </View>
-        <View style={{ flex: 2, height:"100%", backgroundColor: "darkorange" }}>
-          <Text>Orange</Text>
-        </View>
-
-      </View>
-      
+      </ImageBackground>
     </SafeAreaProvider>
     
   );
@@ -37,5 +40,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center"
   },
 });
