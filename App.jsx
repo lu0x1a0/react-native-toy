d25a094e3e2af359820f9aea342008fc86147477
Sean Luo's Yoga <41530060+lu0x1a0@users.noreply.github.com>
2021-11-20 13:17:24 +1100

    timer thingy

--- App.jsx
+++ App.jsx
@@ -8,25 +8,32 @@ export default function App() {
     <SafeAreaProvider>
       <View style={[styles.container, {
           // Try setting `flexDirection` to `"row"`.
-          flexDirection: "column"
+          flexDirection: "row"
         }]}>
-        <View style={{ flex: 1, backgroundColor: "red" }} />
-        <View style={{ flex: 2, backgroundColor: "darkorange" }} />
-        <View style={{ flex: 3, backgroundColor: "green" }} />
-        <View>
-          <Home time = "2"/>
+        <View style={{ flex: 2, height:"100%", backgroundColor: "red" }}>
+          <Text>Red</Text>
         </View>
+        <View style={{ flex: 6, height:"100%", backgroundColor: "green" }}>
+          <Home time="2"/>
+        </View>
+        <View style={{ flex: 2, height:"100%", backgroundColor: "darkorange" }}>
+          <Text>Orange</Text>
+        </View>
+
       </View>
       
     </SafeAreaProvider>
     
   );
+  //<View>
+  //  <Home time = "2"/>
+  //</View>
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    //padding: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -2,25 +2,32 @@ import React, { useEffect, useState } from "react";
 import {Text,Button, View} from 'react-native';
 export default function Home(props){
     const [timerCount, setTimer] = useState(parseInt(props.time))
+    const [timepair,setTimePair] = useState([[0,0]])
     const [start,setStart] = useState(false)
     useEffect(() => {
         let interval = setInterval(() => {
-        setTimer(lastTimerCount => {
-            //lastTimerCount <= 0 && clearInterval(interval);
-            //console.log(lastTimerCount)
-            if (lastTimerCount === 0){
-                return props.time
-            } else{
-                return lastTimerCount - 1
-            }
-        })
-      }, 1000) //each count lasts for a second
-      //cleanup the interval on complete
-      return () => clearInterval(interval)
+            setTimer(lastTimerCount => {
+                //lastTimerCount <= 0 && clearInterval(interval);
+                //console.log(lastTimerCount)
+                if (lastTimerCount === 0){
+                    return props.time
+                } else{
+                    return lastTimerCount - 1
+                }
+            });
+            setTimePair(lasttimepair=>{
+                let timepair = JSON.parse(JSON.stringify(lasttimepair))
+                timepair.push([timepair[timepair.length-1][0]+1,(new Date()).getTime()]) 
+                return timepair
+            });
+        }, 1000); //each count lasts for a second
+       //cleanup the interval on complete
+        return () => clearInterval(interval)
     }, []);
     return (
         <View>
             <Text>{timerCount}</Text>
+            <Text>{timepair[timepair.length-1][0]} {timepair[timepair.length-1][1]}</Text>
             <Button title = "start" onPress = {()=>setStart(startstatus=>{return !startstatus})}/>
             <Text>{String(start)}</Text>
         </View>
